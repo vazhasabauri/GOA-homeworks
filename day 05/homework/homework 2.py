40cacfd21c474e813457725224d439e2f819e2e9
vazhasabauri <vazhasabauri13@gmail.com>
2025-03-14 20:18:39 +0400

    second commit

--- day 05/homework/homework 2.py
+++ day 05/homework/homework 2.py
@@ -2,32 +2,67 @@
 #book=harry Potter
 #სწორი
 #book=harry potter
-#print("book")
+#print(book)
 
 
 #print(sam)
 #sam=america
 #სწორი
 #sam=america
-#print("sam")
+#print(sam)
 
 
 #thing=table
 #print"thing"
 #სწორი
 #thing=table
-#print("thing")
+#print(thing)
 
 
 #print"boy"
 #boy=danny
 #სწორი
 #boy=danny
-#print("boy")
+#print(boy)
 
 
 #print("sunday")
 #day=sunday
 #სწორი
 #day=sunday
-#print("day")
+#print(day)
+
+
+#girl=luna
+#print("luna")
+#სწორი
+#girl=luna
+#print(girl)
+
+
+#print("human")
+#human=man
+#სწორი
+#human=man
+#print(human)
+
+
+#print("foto")
+#foto=image
+#სწორი
+#foto=image
+#print(foto)
+
+
+#dog=animal
+#print("dog")
+#სწორი
+#dog=animal
+#print(dog)
+
+
+#tree=forest
+#print"tree"
+#სწორი
+#tree=forest
+#print(tree)
