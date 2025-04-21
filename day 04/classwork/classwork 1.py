fc44086d03f20bd8ea03f3411f2510821781702e
vazhasabauri <vazhasabauri13@gmail.com>
2025-04-21 19:20:07 +0400

    second commit

---
+++ day 04/classwork/classwork 1.py
@@ -0,0 +1,5 @@
+num1=10
+num2=20
+
+print((num1) + (num2))
+
---
+++ day 04/classwork/classwork 2.py
@@ -0,0 +1,47 @@
+#1.
+
+#print((num1) + (num2))
+#num1=3
+#num2=4
+#სწორი
+#num1=3
+#num2=4
+#print((num1) + (num2))
+
+#2.
+
+#print((num9) + (num8))
+#num9=100
+#num8=200
+#სწორი
+#num9=100
+#num8=200
+
+#3.
+
+#print((num6) + (num 7))
+#num6=400
+#num7=500
+#სწორი
+#num6=400
+#num7=500
+
+#4.
+
+#print((num3) + (num4))
+#num3=700
+#num4=300
+#სწორი
+#num3=700
+#num4=300
+#print((num3) + (num4))
+
+#5.
+
+#print((num2) + (num1))
+#num2=300
+#num1=400
+#სწორი
+#num2=300
+#num1=400
+#print((num2) + (num1))
